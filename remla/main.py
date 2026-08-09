0fb40edb214eef66235c6b02f91322f1a531ad10
Zak Espley <zak.espley@gmail.com>
2026-08-09 14:18:45 -0700

    Version updated

--- remla/main.py
+++ remla/main.py
@@ -27,7 +27,7 @@ from remla.yaml import createDevicesFromYml, yaml
 
 from .customvalidators import *
 
-__version__ = "0.3.3.dev7"
+__version__ = "0.3.3.dev8"
 
 
 def version_callback(value: bool):
