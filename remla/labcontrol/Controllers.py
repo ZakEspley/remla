9f21dc94e77e659423e23b7ad94798ab9b781a37
Zak Espley <zak.espley@gmail.com>
2026-08-17 00:00:28 -0700

    Accept camera switch mode in Arducam controller

--- remla/labcontrol/Controllers.py
+++ remla/labcontrol/Controllers.py
@@ -1420,6 +1420,7 @@ class ArduCamMultiCamera(BaseController):
         streamPath="cam",
         mediamtxApiUrl="http://127.0.0.1:9997",
         controlTimeout=2,
+        cameraSwitchMode="hot",
     ):
         super().__init__(name)
         self.videoNumber = videoNumber
@@ -1433,6 +1434,7 @@ class ArduCamMultiCamera(BaseController):
         self.streamPath = streamPath
         self.mediamtxApiUrl = mediamtxApiUrl
         self.controlTimeout = controlTimeout
+        self.cameraSwitchMode = str(cameraSwitchMode).strip().lower()
 
         # Define Pins
         # Board Pin 7 = BCM Pin 4 = Selection
